f2037850c2aaeae6cf1fc5d5338c1899ba537ebf
ssleert <semenilichevsky@gmail.com>
2024-06-23 23:54:08 +0300

    init

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp $HOME/vimwiki_html/index.html ./index.html
+pandoc index.html --pdf-engine=wkhtmltopdf --metadata title="" -o index.pdf --css=./pdf-style.css
+cp ./index.pdf ./cv.pdf
