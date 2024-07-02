13ecc6238e6a5e34d26d6b724bb2d3dd57c5fdd0
ssleert <semenilichevsky@gmail.com>
2024-07-02 22:45:57 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 cp $HOME/vimwiki_html/index.html ./index.html
 pandoc index.html --pdf-engine=wkhtmltopdf --metadata title="" -o index.pdf --css=./pdf-style.css
 cp ./index.pdf ./cv.pdf
